80a0a07649d0d52c5b50534d0fda4b0b9b983228
Banin Abrar <abrar@janatechnology.com>
2018-12-02 18:05:04 -0500

    v1

--- init_repo.py
+++ init_repo.py
@@ -13,36 +13,47 @@ TO-DO:
 """
 doc = \
 """
-    Hello JANA Developer!
+##########################################################################
 
-    You are about initialize this repository to use custom JANA git hooks.
-    Hope you're okay with that. Why am I kidding, you have to be okay with it.
-    In any case, if you dont like the hooks, just use the '--no-verify' flag
-    on your commits to exit them. But try not to be too smart.
+Hello JANA Developer!
 
-
-    I am watching.
-
-
-    Git hooks are awesome. They can do all sorts of things on your behalf:
-       - Scan for any accidental hard tabs you placed in the src files
-       - Warn you about any binary file that you may be commiting to the repo.
-       - Notify you about any unintentional file mode changes (very useful for POSIX systems)
-       - Stop you from commiting files that are way too big to swallow.
-
-    Basically blocking common types of undesired commits.
+You are about to:
+    - Configure this repository to use custom JANA git hooks.
+    - Enforce JANA's forking workflow on to your development.
+    - Initialize all dependencies required for J-TIMP development.
+    - Enforce PEP8 styling to any new code written for this repo.
 
 
-    In addtion to custom hooks, I'll take the honor of:
-        - setting your upstream to the proper JANA repository
-        - ensuring that you are following the proper forking workflow
-        - setting up your styling to be consitent with JANA style guide
+Hope you're okay with that. Why am I kidding, you have to be okay with it.
+In any case, if you dont like the hooks, just use the '--no-verify' flag
+on your commits to bypass them. But try not to be too smart,
 
-    And I'm capable of doing all that for both HTTPS and SSH.
 
-    Hoping to see you at work. Just press 'y' below and we're good to go.
+I am watching.
 
+
+Git hooks are awesome. They can do all sorts of things on your behalf:
+   - Scan for any accidental hard tabs you placed in the src files
+   - Warn you about any binary file being commiting to the repo.
+   - Notify you about any unintentional file mode changes. (POSIX)
+   - Stop you from commiting files that are unusually large for the repo.
+
+Basically they block the most common types of undesired commits.
+
+
+In addtion to custom hooks, I'll take the honor of:
+    - setting your upstream to the proper JANA repository
+    - setting up your styling to be consitent with JANA style guide
+
+
+Also, this is a completely SSH friendly script.
+Hoping to see you at work. Just press 'y' below and we're good to go.
+
+PS: I'll install some dependencies first, that may take a minute.
+
+##########################################################################
 """
+# start script on user command
 print(doc)
 yn = input("Proceed ([y]/n)? ")
 if yn.lower() != 'y':
@@ -60,82 +71,78 @@ except AssertionError:
           file=sys.stderr)
     print("You must be running Python 3.6 or newer.", file=sys.stderr)
     raise SystemExit(1)
-# just doing some lazy imports, don't hate me
-print("Initializing dependencies ...")
-print("This may take a while.")
+
 try:
     import subprocess as sp
 except (ModuleNotFoundError, ImportError):
     print('Installing core shell dependencies ...')
     os.system('pip install subprocess')
     import subprocess as sp
-try:
-    from requests import (
-        get,
-        ConnectionError,
-    )
-except (ModuleNotFoundError, ImportError):
-    print('requests library not found. Installing it first ...')
-    sp.run("pip install requests")
-    from requests import (
-        get,
-        ConnectionError,
-    )
-try:
-    from urllib3.exceptions import (
-        NewConnectionError,
-        MaxRetryError,
-    )
-except (ModuleNotFoundError, ImportError):
-    print('urllib3 library not found. Installing it first ...')
-    sp.run("pip install urllib3")
-    from urllib3.exceptions import (
-        NewConnectionError,
-        MaxRetryError,
-    )
-try:
-    from progress.bar import (
-        Bar,
-        ShadyBar,
-    )
-except (ModuleNotFoundError, ImportError):
-    print("Installing progress lib ...")
-    sp.run("pip install progress", stdout=sp.PIPE)
-    from progress.bar import (
-        Bar,
-        ShadyBar,
-    )
-try:
-    from time import sleep
-except (ModuleNotFoundError, ImportError):
-    print('Installing dependencies ...')
-    from time import sleep
-try:
-    from colorama import init
-except (ModuleNotFoundError, ImportError):
-    print("Adding color support for ANSI Terminal ...")
-    sp.run("pip install colorama", stdout=sp.PIPE)
-    from colorama import init
-try:
-    from termcolor import colored
-except (ModuleNotFoundError, ImportError):
-    print("Installing termcolor lib for ANSI Terminal")
-    sp.run("pip install termcolor", stdout=sp.PIPE)
-    from termcolor import colored
+
+dependencies = [
+    'requests',
+    'urllib3',
+    'progress',
+    'colorama',
+    'termcolor'
+]
+
+# just doing some lazy imports, don't hate me
+print("Initializing dependencies ...")
+print("This may take a while.")
+print('------------------------')
+
+# get a list of currently installed packages
+reqs = sp.check_output([sys.executable, '-m', 'pip', 'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+# install all packages that aren't installed
+for pkg in dependencies:
+    if pkg not in installed_packages:
+        print('Installing {}'.format(pkg))
+        pkg_cmd = "pip install " + pkg
+        sp.run(pkg_cmd)
+
+# upgrade pip to use the dependency list propely
+print("Upgrading pip ...")
+sp.run("python -m pip install --upgrade pip")
+
+# msgpack may not be available after a fresh python installation
+# no need to import it however
+print("Updrading msgpack ...")
+sp.run("pip install --upgrade msgpack")
+
+from requests import (
+    get,
+    ConnectionError,
+)
+from urllib3.exceptions import (
+    NewConnectionError,
+    MaxRetryError,
+)
+from progress.bar import (
+    Bar,
+    ShadyBar,
+)
+from time import sleep
+from colorama import init
+from termcolor import colored
 
 # add color support
 init()
 # successful import
+print('------------------------')
 print(colored('Done', 'green'))
+sleep(0.2)
 
 # visual confirmating of passing version requirement
 # actual test is done at top of file
-sys.stdout.write('Checking Python Verison ... ')
+sys.stdout.write('\nChecking Python Verison ... ')
 sys.stdout.flush()
-sleep(1)
+sleep(0.5)
 sys.stdout.write(colored('Done\n', 'green'))
 sys.stdout.flush()
-sleep(0.3)
+sleep(0.5)
 
 # state flags
 flags = {
@@ -178,11 +185,11 @@ def create_template_dir():
     """Create git template dir and hook dir if those don't exist"""
     hook_dir = get_hook_dir()
     mkd = "mkdir " + hook_dir
-    print("\n\n>>Creating git template directory ...")
+    sys.stdout.write("Creating git template directory ...\n")
+    sys.stdout.flush()
     command = sp.run(mkd, stdout=sp.PIPE, shell=True)
     if command.returncode != 1:
-        msg = "Created git template directory. Setting up hooks ..."
-        print(colored('Success:', 'green' ),msg)
+        sys.stdout.write(colored('Done\n', 'green' ),msg)
     else:
         msg = "Git template directory exists." + \
             " Using exsisting directory. \n"
@@ -224,7 +231,7 @@ def dl_file(url, **kwargs):
     if kwargs['file'] == 'pre-commit':
         file_name = os.path.join(get_hook_dir(), "pre-commit")
         # in odrer to write 'Done' inline
-        sys.stdout.write('\n>>Writing response to file ... ')
+        sys.stdout.write('\nWriting response to file ... ')
         sys.stdout.flush()
         with open(file_name, "wb") as file:
             file.write(response.content)
@@ -232,52 +239,19 @@ def dl_file(url, **kwargs):
     elif kwargs['file'] == '.editorconfig':
         file_name = os.path.join(os.getcwd(), ".editorconfig")
         # in odrer to write 'Done' inline
-        sys.stdout.write('\n>>Writing response to file ... ')
+        sys.stdout.write('\nWriting response to file ... ')
         sys.stdout.flush()
         with open(file_name, "wb") as file:
             file.write(response.content)
-
-
-def init_hooks_remote(url):
-    """Main function to initialize hooks and set remote"""
-    # check if current dir is a git repository
-    if not os.path.isdir(".git"):
-        error_msg = "Fatal: Not a git repository." + \
-            " Run script from the root of a git repository"
-        print(error_msg)
-        raise SystemExit(1)
-    # create the git template directory
-    create_template_dir()
-    # download git hooks from url
-    print(">>Fetching pre-commit hooks from build_support ...")
-    dl_file(url, file='pre-commit')
+    # delays for visual consistency
     sleep(0.5)
-    sys.stdout.write(colored('Done\n\n', 'green'))
-    # Set the newly created the git_template as the template dir
-    cmd = "git config --global init.templatedir " + get_template_dir()
-    sp.run(cmd)
-
-    # Update the current repo by removing old hooks
-    # and running 'git init' in .git dir
-    if os.path.isfile(r".git\hooks\pre-commit"):
-        cmd = r"del .git\hooks\pre-commit"
-        sp.run(cmd, shell=True)
-        print(">>Removing old hooks from repository.")
-    # Replace existing hooks with newer ones
-    # and reinitialize repository
-    cmd = "git init"
-    git_init = sp.run(cmd, stdout=sp.PIPE)
-    if git_init.returncode == 0:
-        print("Reinitialized existing git repository in\n\n", "    {} \n\n"
-              .format(os.getcwd()))
-
-    # add upstream for the repository
-    add_upstream_remote()
-
+    sys.stdout.write(colored("Done", 'green'))
+    sys.stdout.flush()
+    print('\n')
 
 def init_editorconfig(ec_url):
     """Initialize editorconfig if it doesn't exist"""
-    print(">>Searching for an exisiting .editorconfig in repo root ...")
+    print("Searching for an exisiting .editorconfig in repo root ...")
     if not os.path.isfile(".editorconfig"):
         # Then download the editorconfig
         print("No existing .editorconfig file found.")
@@ -285,14 +259,18 @@ def init_editorconfig(ec_url):
         dl_file(ec_url, file='.editorconfig')
         print('Success: initialized .editorconfig into {}\n'
               .format(os.getcwd()))
+    # if editorconfig file already exists
     print("Repository is already initialized with .editorconfig. Skipping.\n")
+    sleep(0.5)
     # Check if Atom is installed
-    print(">>Searching for an instance of Atom Text Editor installation ...")
+    print("Searching for an instance of Atom Text Editor installation ...")
+    sleep(0.5)
     check = sp.run("atom -v", stdout=sp.PIPE, shell=True)
     if check.returncode == 0:
-        print(colored("Atom Text Editor installation found.\n\n", "green"))
+        print(colored("Atom Text Editor installation found.", "green"))
+        sleep(0.2)
         # install editorconfig plugin for atom
-        print(">>Installing EditorConfig plugin for Atom Text Editor ...")
+        print("\nInstalling EditorConfig plugin for Atom Text Editor ...")
         print("Note: If progress bar is idle for more than 30 seconds,",
               "hit Ctrl-C once to continue.")
         bar = ShadyBar('\rInstalling', max=10, suffix='%(percent)d%%')
@@ -318,7 +296,7 @@ def init_editorconfig(ec_url):
             print(relax, colored("https://editorconfig.org", 'cyan'))
         else:
             editorconfig_plugin = True
-            print(colored('\nSuccess: EditorConfig configured!', 'white'))
+            print(colored('\nSuccess:', 'green'), 'EditorConfig configured!')
             print('NOTE: To use EditorConfig with other IDEs, visit',
                   colored("https://editorconfig.org\n", 'cyan'))
     else:
@@ -335,7 +313,10 @@ def add_upstream_remote():
     and add it to the upstream. Works for both HTTPS and SSH"""
     cmd = 'git config --get remote.origin.url'
     # [:-1] is used to remove carriage return
+    sys.stdout.write("Configuring upstream to be JANA remote ...\n")
+    sys.stdout.flush()
     fork = sp.run(cmd, stdout=sp.PIPE).stdout.decode('utf-8')[:-1]
+    sleep(1.5)
     # check if url is SSH or HTTPS
     isSSH = False
     if fork.startswith("git@"):
@@ -348,6 +329,8 @@ def add_upstream_remote():
         raise SystemExit(1)
     # obtain JANA url from SSH or HTTPS
     if not isSSH:
+        print("HTTPS remote detected. Using HTTPS.")
+        sleep(0.2)
         fork = fork.split('/')
         # if using main repo instead of fork, throw error
         if fork[-2] == 'JANA-Technology':
@@ -359,6 +342,8 @@ def add_upstream_remote():
         jana_remote = '/'.join(fork)
 
     else:  # if isSSH
+        print("SSH remote detected. Using SSH.")
+        sleep(0.2)
         fork = fork.split('/')
         fork_inner = fork[0].split(':')
         if fork_inner[1] == "JANA-Technology":
@@ -371,27 +356,83 @@ def add_upstream_remote():
         jana_remote = '/'.join(fork)
 
     # add the jana remote as upstream
-    print(">>Configuring upstream to be JANA remote ...")
     cmd = 'git remote add upstream ' + jana_remote
     add_remote = sp.run(cmd, stdout=sp.PIPE)
     if add_remote.returncode == 0:
-        print("    Set upstream as JANA repository:\n")
+        print(colored('Success:', 'green'),
+                "Set upstream as JANA repository:\n")
         print('        {}' .format(jana_remote))
     # if upstream remote already exists, overwrite it
     elif add_remote.returncode == 128:
-        msg = "Upstream remote already exists. " + \
-            "-> Updating it to JANA repository."
+        msg = "Upstream remote already exists.\n" + \
+            "            -> Updating it to JANA repository.\n"
         print(colored('Warning:', 'yellow'), msg)
+        sleep(0.2)
         cmd = 'git remote set-url upstream ' + jana_remote
         sp.run(cmd, stdout=sp.PIPE)
-        msg = "Success: Updated upstream remote to JANA repository.\n"
-        print(colored(msg, 'white'))
+        msg = "Updated upstream remote to JANA repository.\n"
+        print(colored('Success:', 'green'),msg)
         print('        {}\n\n' .format(jana_remote))
     # Unknwon error. Stop script
     else:
         error_msg = "    Warning: Could not set upstream remote." + \
             " You should set it manually to be the JANA repository"
         print(error_msg, stdout=sys.stderr)
+
+
+def init_all(url, ec_url):
+    """Main function to initialize hooks, set remote
+    and configure EditorConfig sytling for current repo"""
+    # check if current dir is a git repository
+    if not os.path.isdir(".git"):
+        error_msg = "Not a git repository." + \
+        " Run script from the root of a git repository."
+        print(colored('Fatal:', 'red'), error_msg)
+        raise SystemExit(1)
+
+    # create the git template directory
+    print('### TEMPLATE DIRECTORY SET UP ###')
+    create_template_dir()
+    sleep(1)
+
+    # download git hooks from url
+    print('### HOOK INITIALIZATION ###')
+    print("Fetching pre-commit hooks from build_support ...")
+    dl_file(url, file='pre-commit')
+    sleep(1)
+
+    # Set the newly created the git_template as the template dir
+    cmd = "git config --global init.templatedir " + get_template_dir()
+    sp.run(cmd)
+
+    # Update the current repo by removing old hooks
+    # and running 'git init' in .git dir
+    print("### OVERWRITING OLD HOOKS ###")
+    if os.path.isfile(r".git\hooks\pre-commit"):
+        sys.stdout.write("Removing old hooks from repository ... ")
+        sys.stdout.flush()
+        cmd = r"del .git\hooks\pre-commit"
+        sp.run(cmd, shell=True)
+        sys.stdout.write(colored('Done', 'green'))
+        sleep(0.5)
+    # Replace existing hooks with newer ones
+    # and reinitialize repository
+    cmd = "git init"
+    git_init = sp.run(cmd, stdout=sp.PIPE)
+    if git_init.returncode == 0:
+        print("\nReinitialized existing git repository in\n\n", "    {} \n\n"
+              .format(os.getcwd()))
+        sleep(0.5)
+
+    # add upstream for the repository
+    print("### SETTING UPSTREAM REMOTE ###")
+    add_upstream_remote()
+    sleep(0.5)
+
+    # init editorconfig
+    print("### INITIALIZING EDITORCONFIG ###")
+    init_editorconfig(ec_url)
+    sleep(0.5)
 
 
 def print_status(flags):
@@ -401,11 +442,14 @@ def print_status(flags):
 
 if __name__ == '__main__':
 
-    print("\nReading System Variables and initializing script ..." )
+    print("\nReading System Variables and initializing script ...\n" )
     # provide time buffer to cancel script
 
     hook_url = 'https://raw.githubusercontent.com/b-abrar/build_support/master/pre-commit.pl'
     ec_url = 'https://raw.githubusercontent.com/b-abrar/build_support/master/.editorconfig'
-    init_hooks_remote(hook_url)
-    init_editorconfig(ec_url)
+    init_all(hook_url, ec_url)
+
+    print('### TIMP REQUIREMENTS INSTALLATION ###')
+    print("Repo initialized.\nNow running install_requirements.py ...\n")
+
     print("...You are all set!... (This is a lazy message, I'll update it with a status table later)")
